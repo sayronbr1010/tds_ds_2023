6613fdba8003f17f74510e557654e8562253c58a
sayronbr1010 <98848566+sayronbr1010@users.noreply.github.com>
2023-03-23 19:03:54 -0300

    Add files via upload

---
+++ LITA MERCADO/script.js
@@ -0,0 +1,62 @@
+const input = window.document.getElementById("input");
+const ul = window.document.querySelector("ul");
+const db_lista_li = JSON.parse(window.localStorage.getItem("lista"));
+var conta = document.write("<h1>Tamanho: "+db_lista_li.length +"</h1>");
+
+
+db_lista_li.forEach((item) => adicionar(item));
+
+function adicionar(item) {
+  var li = window.document.createElement("li");
+  var value = item ? item.texto : input.value;
+  var value_li = window.document.createTextNode(value);
+ 
+
+
+  if (input.value.trim() !== "" || item) {
+    li.appendChild(value_li);
+    ul.appendChild(li);
+  }
+
+  if (!item) {
+    adiciona_lista();
+  }
+  if(item){
+    if(item.checked){
+      li.setAttribute("class","checked")
+    }
+  }
+
+  input.value = "";
+  input.focus();
+}
+
+window.document.querySelector("ul").addEventListener("click", (e) => {
+  e.target.classList.toggle("checked");
+  adiciona_lista();
+});
+
+window.document.getElementById("input").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    adicionar();
+  }
+});
+
+window.document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  if (e.target.tagName === "LI") {
+    e.target.remove();
+    adiciona_lista();
+  }
+});
+
+function adiciona_lista() {
+  const lista_de_li = window.document.querySelectorAll("li");
+  const lista = [];
+  
+  lista_de_li.forEach((item) => {
+    lista.push({ texto: item.innerText, checked: item.className =="checked" });
+  });
+
+  window.localStorage.setItem("lista", JSON.stringify(lista));
+}
